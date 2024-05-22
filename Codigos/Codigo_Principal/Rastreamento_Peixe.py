40f2264b635090ee85f0f517e62f0f67282e7d1a
Vinicius Soares <62343620+vncs-srs@users.noreply.github.com>
2024-05-22 19:06:39 -0300

    movimento de teste do carro funcionado

--- Codigos/Codigo_Principal/Rastreamento_Peixe.py
+++ Codigos/Codigo_Principal/Rastreamento_Peixe.py
@@ -4,20 +4,22 @@ COR_VERMELHO = (0, 0, 255)
 COR_VERDE = (0, 255, 0)
 COR_AZUL = (255, 0, 0)
 
-LIMITE_INFERIOR = (90, 50, 50) 
-LIMITE_SUPERIOR = (130, 255, 255) 
-
 
 class Rastreamento_Peixe:
 
     def __init__(self):
         self.kernel_size = (5, 5)
         self.epsilon_multiplicador = 0.001
-        self.range_cor = [LIMITE_INFERIOR, LIMITE_SUPERIOR]
+        self.LIMITE_INFERIOR = (90, 50, 50)
+        self.LIMITE_SUPERIOR = (130, 255, 255)
+
+    def definir_limites_cor(self, limite_inferior, limite_superior):
+        self.LIMITE_INFERIOR = limite_inferior
+        self.LIMITE_SUPERIOR = limite_superio
 
     def linearizar_frame(self, frame):
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        black_mask = cv2.inRange(hsv, self.range_cor[0], self.range_cor[1])
+        black_mask = cv2.inRange(hsv, self.LIMITE_INFERIOR, self.LIMITE_SUPERIOR)
         black_result = cv2.bitwise_and(frame, frame, mask=black_mask)
         gray_frame = cv2.cvtColor(black_result, cv2.COLOR_BGR2GRAY)
         gaus_frame = cv2.GaussianBlur(gray_frame, self.kernel_size, 0)
@@ -26,33 +28,42 @@ class Rastreamento_Peixe:
         frame = cv2.morphologyEx(frame_binario, cv2.MORPH_CLOSE, pixel_preenchimento)
         return frame
 
-    def trancking_peixe(self, frame, frame_binario):
+    def calcular_area_contorno(self, contorno):
+        return cv2.contourArea(contorno)
+
+    def tracking_peixe(self, frame, frame_binario):
         contornos, _ = cv2.findContours(frame_binario, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         if contornos:
-            max_Area = cv2.contourArea(contornos[0])
-            contorno_Max_Area_Id = 0
-            for i, contorno in enumerate(contornos):
-                if max_Area < cv2.contourArea(contorno):
-                    max_Area = cv2.contourArea(contorno)
-                    contorno_Max_Area_Id = i
-            maior_Contorno = contornos[contorno_Max_Area_Id]
+            maior_contorno = max(contornos, key=self.calcular_area_contorno)
             self.desenha_grade(frame)
-            self.desenha_tracking(frame, maior_Contorno)
+            self.desenha_tracking(frame, maior_contorno)
 
-    def rastreia_ponto(self, frame, contorno):
+    def desenha_tracking(self, frame, contorno):
         hull = cv2.convexHull(contorno)
         cv2.drawContours(frame, [hull], -1, COR_AZUL, 1)
         M = cv2.moments(hull)
-        x_Central = int(M["m10"] / M["m00"])
-        y_Central = int(M["m01"] / M["m00"])
-        return x_Central, y_Central
+        if M["m00"] != 0:
+            x_central = int(M["m10"] / M["m00"])
+            y_central = int(M["m01"] / M["m00"])
+            cv2.circle(frame, (x_central, y_central), 5, COR_VERMELHO, -1)
+            cv2.drawContours(frame, [contorno], -1, (0, 255, 255), 1)
+            texto = f"X: {x_central}, Y: {y_central}"
+            cv2.putText(frame, texto, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
 
-    def desenha_tracking(self, frame, contorno):
-        x_Central, y_Central = self.rastreia_ponto(frame, contorno)
-        cv2.circle(frame, (x_Central, y_Central), 5, COR_VERMELHO, -1)
-        cv2.drawContours(frame, [contorno], -1, (0, 255, 255), 1)
-        texto = f"X: {x_Central}, Y: {y_Central}"
-        cv2.putText(frame, texto, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+#    def rastreia_ponto(self, frame, contorno):
+#        hull = cv2.convexHull(contorno)
+#        cv2.drawContours(frame, [hull], -1, COR_AZUL, 1)
+#        M = cv2.moments(hull)
+#        x_Central = int(M["m10"] / M["m00"])
+#        y_Central = int(M["m01"] / M["m00"])
+#        return x_Central, y_Central
+#
+#    def desenha_tracking(self, frame, contorno):
+#        x_Central, y_Central = self.rastreia_ponto(frame, contorno)
+#        cv2.circle(frame, (x_Central, y_Central), 5, COR_VERMELHO, -1)
+#        cv2.drawContours(frame, [contorno], -1, (0, 255, 255), 1)
+#        texto = f"X: {x_Central}, Y: {y_Central}"
+#        cv2.putText(frame, texto, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
         
     def desenha_grade(self, frame):
         h_Frame, w_Frame, _ = frame.shape
@@ -63,6 +74,46 @@ class Rastreamento_Peixe:
         for i in range(1, divisoes):
             x = int(i * w_Frame / divisoes)
             cv2.line(frame, (x, 0), (x, h_Frame), COR_VERDE, 2)
+   
+    def mover_carro(self, frame, contorno):
+        h_frame, w_frame, _ = frame.shape
+        if contorno is not None:
+            x_central, y_central = self.calcular_centro_contorno(contorno)
+            # Defina as coordenadas dos limites para cada direção
+            x1, x2 = w_frame // 3, 2 * w_frame // 3
+            y1, y2 = h_frame // 3, 2 * h_frame // 3
+
+            # Lógica para movimentar o carro com base na posição do peixe
+            if y1 < y_central < y2:
+                if x1 < x_central < x2:
+                    print("Fique parado")
+                elif x_central >= x2:
+                    print("Ande para a esquerda")
+                else:  # x_central <= x1
+                    print("Ande para a direita")
+            elif y_central >= y2:
+                if x1 < x_central < x2:
+                    print("Ande para cima")
+                elif x_central >= x2:
+                    print("Ande para cima e para a esquerda")
+                else:  # x_central <= x1
+                    print("Ande para cima e para a direita")
+            else:  # y_central <= y1
+                if x1 < x_central < x2:
+                    print("Ande para baixo")
+                elif x_central >= x2:
+                    print("Ande para baixo e para a esquerda")
+                else:  # x_central <= x1
+                    print("Ande para baixo e para a direita")
+
+    def calcular_centro_contorno(self, contorno):
+        M = cv2.moments(contorno)
+        if M["m00"] != 0:
+            x_central = int(M["m10"] / M["m00"])
+            y_central = int(M["m01"] / M["m00"])
+            return x_central, y_central
+        else:
+            return None
 
     def loop(self):
         webcam = cv2.VideoCapture(0)
@@ -74,7 +125,10 @@ class Rastreamento_Peixe:
             valido, frame = webcam.read()
             if valido:
                 frame_binario = self.linearizar_frame(frame)
-                self.trancking_peixe(frame, frame_binario)
+                contornos, _ = cv2.findContours(frame_binario, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+                contorno_principal = max(contornos, key=self.calcular_area_contorno) if contornos else None
+                self.mover_carro(frame, contorno_principal)
+                self.tracking_peixe(frame, frame_binario)
                 cv2.imshow('BINARIO', frame_binario)
                 cv2.imshow('PRINCIPAL', frame)
                 if cv2.waitKey(1) != -1:
