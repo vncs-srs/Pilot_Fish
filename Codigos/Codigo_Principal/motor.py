14ad3ad7812eeb50a3d27cea9ce191c5cdb07c6d
Vinicius Soares <62343620+vncs-srs@users.noreply.github.com>
2024-05-27 16:42:38 -0300

    Parâmetros para diminuir a velocidade das rodas nas cuvas

--- Codigos/Codigo_Principal/motor.py
+++ Codigos/Codigo_Principal/motor.py
@@ -4,12 +4,14 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 class Motor:
-    def __init__(self, p1, p2):
+    def __init__(self, p1, p2, p3):
         self.pino_1 = p1
         self.pino_2 = p2
+        self.pwm = p3
         
         GPIO.setup(self.pino_1, GPIO.OUT)
         GPIO.setup(self.pino_2, GPIO.OUT)
+        GPIO.setup(self.pwm, GPIO.OUT)
         
         GPIO.output(self.pino_1, GPIO.LOW)
         GPIO.output(self.pino_2, GPIO.LOW)
@@ -29,3 +31,15 @@ class Motor:
     def parar(self):
         GPIO.output(self.pino_1, GPIO.LOW)
         GPIO.output(self.pino_2, GPIO.LOW)
+
+    def frente_reduzido(self):
+        pwm = GPIO.PWM(self.pino_1, 1000)
+        pwm.start(0)
+        pwm.ChangeDutyCycle(50)
+        GPIO.output(self.pino_2, GPIO.LOW)
+
+    def re_reduzido(self):
+        pwm = GPIO.PWM(self.pino_2, 1000)
+        pwm.start(0)
+        pwm.ChangeDutyCycle(50)
+        GPIO.output(self.pino_1, GPIO.LOW)
