19deaa1c88bfa0f0b8aa76c4ab4d3d27291b5898
Vinicius Soares <62343620+vncs-srs@users.noreply.github.com>
2024-05-28 14:46:02 -0300

    Create sensorProximidade.py

---
+++ Codigos/Codigo_Principal/sensorProximidade.py
@@ -0,0 +1,15 @@
+from gpiozero import DistanceSensor
+from time import sleep
+
+# Conecte o sensor ultrassônico aos pinos GPIO corretos
+# Por exemplo, TRIG ao pino 23 e ECHO ao pino 24
+sensor = DistanceSensor(echo=25, trigger=8)
+
+try:
+    while True:
+        distance = sensor.distance * 100  # Convertendo para centímetros
+        print(f"Distância: {distance:.2f} cm")
+        sleep(1)  # Aguarda 1 segundo
+
+except KeyboardInterrupt:
+    print("\nPrograma encerrado pelo usuário.")
