0e1c785c63b1d60bd839ac8632598049de613554
Vinicius Soares <62343620+vncs-srs@users.noreply.github.com>
2025-02-17 12:12:52 -0300

    Correção de bugs no sensor de proximidade

--- Codigos/Codigo_Principal/sensorProximidade.py
+++ Codigos/Codigo_Principal/sensorProximidade.py
@@ -2,14 +2,18 @@ import RPi.GPIO as GPIO
 import time
 
 class SensorProximidade:
-    def __init__(self):
-        #self.trig = 6
-        #self.echo = 5
+    def __init__(self, trig, echo):
+        self.trig = trig
+        self.echo = echo
+        
+        # Configuração dos pinos
         GPIO.setup(self.trig, GPIO.OUT)
         GPIO.setup(self.echo, GPIO.IN)
+        GPIO.output(self.trig, False)
+        time.sleep(0.1)  # Pequena pausa para estabilizar
 
     def medir_distancia(self):
-        # Enviar um pulso de 10us
+        # Enviar um pulso de 10µs para ativar o sensor
         GPIO.output(self.trig, True)
         time.sleep(0.00001)
         GPIO.output(self.trig, False)
@@ -17,18 +21,18 @@ class SensorProximidade:
         inicio_tempo = time.time()
         fim_tempo = time.time()
 
-        # Salva o tempo de envio do pulso
+        # Espera o ECHO ficar alto
         while GPIO.input(self.echo) == 0:
             inicio_tempo = time.time()
 
-        # Salva o tempo de recepção do pulso
+        # Espera o ECHO ficar baixo
         while GPIO.input(self.echo) == 1:
             fim_tempo = time.time()
 
-        # Calcular a diferença de tempo
+        # Calcula a diferença de tempo
         duracao = fim_tempo - inicio_tempo
 
-        # Multiplica pela velocidade do som (34300 cm/s) e divide por 2
+        # Converte para distância em cm (velocidade do som = 34300 cm/s)
         distancia = (duracao * 34300) / 2
 
         return distancia
